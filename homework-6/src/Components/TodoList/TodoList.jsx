67dbc8ad1e6417893f19a9c36a2e468cdb84b03a
RENATA_PRO <renatakov1010@gmail.com>
2023-07-23 20:02:04 +0200

    switch case doesn't work

--- homework-6/src/Components/TodoList/TodoList.jsx
+++ homework-6/src/Components/TodoList/TodoList.jsx
@@ -1,59 +1,60 @@
 import Button from "../assets/Button";
-import TodoListItem from "./TodoListItem"
+// import TodoListItem from "./TodoListItem"
 import s from "./TodoList.module.css"
 import { useRef } from "react";
 
 const ToDoList = (props) =>{
+    const deleteBtnRef = useRef(null)
     
-    let listInfo = []
-    switch(props.filter){
-        case 'Active': {
-            // console.log('active')
-            listInfo = props.tasks.filter((taskItem)=>{
-            if(taskItem.status === "Active"){
-                return taskItem
-                } else{
-                    return null
-                }
-            })
-            break
-            }
-            case 'Completed': {
-                listInfo = props.tasks.filter((taskItem)=>{
-                if(taskItem.status === "Completed"){
-                    return taskItem
-                    } else{
-                        return null
-                    }
-                })
-                break
-                }
-            default: {
-                listInfo = props.tasks
-                break
-            }
-        }
-        const ListCollection = listInfo.map((listItem)=>{
-            return <TodoListItem
-            id={listItem.id}
-            isActive={listItem.isActive}
-            deleteBtn={props.deleteToDo()}
-            key={listItem}
-            status={listItem.status}
-            text={listItem.title}
-            />
-        })
-    // const getAllTodos = props.tasks.map(task =>{
-    //     return(
-    //         <div className={s.taskContainer}>
-    //             <input checked={task.isActive === false} type="checkbox" />
-    //             <h3>
-    //                 {task.title}
-    //             </h3>
-    //             {task.status === "Active" ? <Button text="X" handleClick={()=>{props.deleteToDo(task.id)}} ref={deleteBtnRef}/> : null}
-    //         </div>
-    //     )
-    // })
+    // let listInfo = []
+    // switch(props.filter){
+    //     case 'Active': {
+    //         // console.log('active')
+    //         listInfo = props.tasks.filter((taskItem)=>{
+    //         if(taskItem.status === "Active"){
+    //             return taskItem
+    //             } else{
+    //                 return null
+    //             }
+    //         })
+    //         break
+    //         }
+    //         case 'Completed': {
+    //             listInfo = props.tasks.filter((taskItem)=>{
+    //             if(taskItem.status === "Completed"){
+    //                 return taskItem
+    //                 } else{
+    //                     return null
+    //                 }
+    //             })
+    //             break
+    //             }
+    //         default: {
+    //             listInfo = props.tasks
+    //             break
+    //         }
+    //     }
+        // const ListCollection = listInfo.map((listItem)=>{
+        //     return <TodoListItem
+        //     id={listItem.id}
+        //     isActive={listItem.isActive}
+        //     deleteBtn={props.deleteToDo()}
+        //     key={listItem}
+        //     status={listItem.status}
+        //     text={listItem.title}
+        //     />
+        // })
+    const getAllTodos = props.tasks.map(task =>{
+        return(
+            <div className={s.taskContainer}>
+                <input checked={task.isActive === false} type="checkbox" />
+                <h3>
+                    {task.title}
+                </h3>
+                {task.status === "Active" ? <Button text="X" handleClick={()=>{props.deleteToDo(task.id)}} ref={deleteBtnRef}/> : null}
+            </div>
+        )
+    })
     console.log(props.tasks)
     const inputTitleRef = useRef(null)
     const btnRef = useRef(null)
@@ -64,7 +65,7 @@ const ToDoList = (props) =>{
     }
     return(
         <>
-        {ListCollection}
+        {getAllTodos}
         <div className={s.addTodoForm}>
             <input className={s.addTodoInput} ref={inputTitleRef} placeholder="Write Task Title"/>
             <Button text="Add Todo" handleClick={handleBtn} ref={btnRef} className="addTodo"/>
